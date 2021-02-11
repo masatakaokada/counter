88141eaf0bf19ccd1167ae57fb000b031a6420d3
Masataka Okada <masataka.okada1117@gmail.com>
2021-02-11 18:41:00 +0900

    counter-appの作成

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CounterContext from './contexts/counter'
+import Counter from './components/counter'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+
+    this.state = {
+      count: 0,
+      increment: this.increment,
+      decrement: this.decrement
+    }
+  }
+
+  increment() {
+    this.setState({count: this.state.count + 1})
+  }
+
+  decrement() {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <CounterContext.Provider value={this.state}>
+        <Counter/>
+      </CounterContext.Provider>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +40,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
